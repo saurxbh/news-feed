7de611707126580d371ae71676236a8ec13da6f6
saurxbh <36542272+saurxbh@users.noreply.github.com>
2024-10-12 21:13:58 -0400

    Set Up Routing

--- src/components/App.js
+++ src/components/App.js
@@ -1,19 +1,27 @@
 import { Container } from "react-bootstrap";
 import Signup from "./Signup";
 import { AuthProvider } from "../contexts/AuthContext";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Dashboard from "./Dashboard";
 
 function App() {
   return (
-    <AuthProvider>
       <Container 
       className="d-flex align-items-center justify-content-center"
       style={{ minHeight: "100vh" }}  
       >
         <div className="w-100" style={{ maxWidth: "400px"}}>
-          <Signup />
+          <Router>
+            <AuthProvider>
+              <Routes>
+                <Route exact path="/" element={<Dashboard />}/>
+                <Route path="/signup" element={<Signup />} />
+                <Route path="/login" element={<Login />} />
+              </Routes>
+            </AuthProvider>
+          </Router>
         </div>
       </Container>
-    </AuthProvider>
   )
 }
 
